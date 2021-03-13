d9b1fc2e0da570fb7f6c0e51fd6653559a7959a9
tphjsz <tphj@outlook.com>
2021-03-13 19:44:06 -0300

    haaa

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -16,7 +16,7 @@
 		var remainingSec = $('.countdown').data('remaining-sec');
 		$('.countdown').ClassyCountdown({
 			theme: "flat-colors-very-wide",
-			end: $.now() + 15000
+			end: $.now() + 999999999
 		});
 	}
 	
